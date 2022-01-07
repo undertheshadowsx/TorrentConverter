b6f3d73517b9156a2cd17b86f9adb0f95c3d3434
shadowhunter777 <89256295+shadowhunter777@users.noreply.github.com>
2022-01-07 23:00:41 +0545

    Update commands.js

--- helpers/commands.js
+++ helpers/commands.js
@@ -5,21 +5,13 @@ startMsg = async (ctx) =>
     ctx.reply(`👋 Hey ${ctx.from.first_name} ..\n\nWith me you can convert TORRENT file to MAGNET Link & vice versa.\nI can handle groups also.\n\nCommands: /help /about\n`,
     {
         parse_mode: "HTML",
-        reply_to_message_id: ctx.message.message_id,
-        reply_markup:{
-            inline_keyboard: [
-                [
-                    {text: '🧑‍💻 Dev', url:'https://t.me/WideWine'},
-                    {text: '🌟 Source Code 🌟', url:'https://github.com/JinsoRaj/TorrentConverter'}
-                ]
-            ]
-        } 
+        reply_to_message_id: ctx.message.message_id
     })
 };
 
 helpMsg = async (ctx) =>
 {
-    ctx.reply(`I can convert magnet links to torrent files and vice versa.\nI can manage your torrent / leech groups also. <a href="https://t.me/TorrentToMagnetRobot?startgroup=start">Add me to your groups.</a>`,
+    ctx.reply(`I can convert magnet links to torrent files and vice versa.\nI can manage your torrent / leech groups also. `,
     {
         parse_mode: "HTML",
         disable_web_page_preview: true,
@@ -29,18 +21,11 @@ helpMsg = async (ctx) =>
 
 aboutMsg = async (ctx) =>
 {
-    ctx.reply(`Developer: @WideWine\nFramework: <a href="https://grammy.dev">grammY</a> 💙\nSource Code: https://github.com/JinsoRaj/TorrentConverter`,
+    ctx.reply(`Framework: <a href="https://grammy.dev">grammY</a> `,
     {
         parse_mode: "HTML",
         disable_web_page_preview: true,
-        reply_to_message_id: ctx.message.message_id,
-        reply_markup:{
-            inline_keyboard: [
-                [
-                    {text: '🌟 Rate Me 🌟', url:'https://t.me/tlgrmcbot?start=torrenttomagnetrobot-review'}
-                ]
-            ]
-        } 
+        reply_to_message_id: ctx.message.message_id
     })
 };
 
